210e9651ea3f664b1575f0edf6c93ec96c67ba40
last-Kazekage <129960474+last-Kazekage@users.noreply.github.com>
2023-04-14 06:00:37 -0700

    multiply using mul

---
+++ 4.multiply-mul.asm
@@ -0,0 +1,12 @@
+#multiplication using mul
+.data
+#directly passing value into text section
+.text
+	addi $s0, $zero, 10
+	addi $s1, $zero, 20
+	
+	mul $t0, $s0, $s1
+	
+	li $v0, 1
+	add $a0, $zero, $t0
+	syscall
